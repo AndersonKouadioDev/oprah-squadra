dc91805e1c9e474f355789b1957c8b1bb44d9024
Anderson Kouadio <andersonkouadio0118@gmail.com>
2024-11-01 15:54:03 +0000

    video

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,13 @@
-import { withNextVideo } from "next-video/process";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-          {
-            protocol: 'https',
-            hostname: 'images.unsplash.com',
-          },
-        ],
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
       },
+    ],
+  },
 };
 
-export default withNextVideo(nextConfig);
+export default nextConfig;
